3866dc5f3f2f0f6124dfab5db39c47a9f3ec3054
H Pradeep <121169489+pradeepDu@users.noreply.github.com>
2024-08-18 17:27:35 +0530

    Add files via upload

---
+++ src/features/feature.d.ts
@@ -0,0 +1,9 @@
+declare module './feature' {
+    export const Navbar: any;
+    export const Welcome: any;
+    export const Footer: any;
+    export const Transaction: any;
+    export const Service: any;
+    export const Loader: any;
+  }
+  
---
+++ src/utility/constants.js
@@ -0,0 +1,3 @@
+import abi from './Transactions.json';
+export const contractABI = abi.abi;
+export const contractAddress ='0xffFB056baFf0755EA05Bd3B085396630C146A7db';
